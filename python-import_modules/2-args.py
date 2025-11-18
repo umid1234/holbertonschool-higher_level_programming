abfeb846dcc16298c89693e3525a6f834bd1fa34
umid1234 <umidaso1357@gmail.com>
2025-11-18 12:29:40 +0000

    task3

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    length = len(argv)-1
+    if(length == 0):
+        print("0 arguments.")
+    elif(length == 1):
+        print("{} argument:".format(length))
+    else:
+        print("{} arguments:".format(length))
+        
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
+
