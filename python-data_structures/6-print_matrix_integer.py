147ce1b7ab362092b1fcb8647131040440b54bcf
umid1234 <umidaso1357@gmail.com>
2025-11-19 13:24:31 +0000

    task

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end = " ")
+        for j in range(len(i)):
+            if j != 0:
+                print(" ", end="")
+            print("{:d}".format(i[j]), end="")
         print()
