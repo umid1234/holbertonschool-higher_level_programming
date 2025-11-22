aaa18d8c7a8056e4aeebe2d591b661477c80c3fa
umid1234 <umidaso1357@gmail.com>
2025-11-22 14:44:13 +0000

    update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print(my_list[i], end = "")
+            print(my_list[i], end="")
             count += 1
-        except:
+        except IndexError:
             break
     print()
     return count
