248cca40bbee7f670cfe53631a15b0f5cd998fc9
umid1234 <umidaso1357@gmail.com>
2025-11-26 15:30:26 +0400

    update

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -3,10 +3,10 @@
 Module-1 Defines Rectangle in 0-rectangle.py
 just empty class
 """
+
+
 class Rectangle:
     """
     Docstring for Rectangle
-    def __init__(self, size=0):
-        self.size = size
     """
     pass
