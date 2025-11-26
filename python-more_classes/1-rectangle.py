8b2a68cb725afd7532131abb390ddf0743fd7d92
umid1234 <umidaso1357@gmail.com>
2025-11-26 18:06:56 +0000

    exception_handling

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -26,9 +26,9 @@ class Rectangle:
     def height(self, value):
         
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
         
     def __init__(self, width=0, height=0):
