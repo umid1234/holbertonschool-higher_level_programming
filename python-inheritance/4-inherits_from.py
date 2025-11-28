99921ecbee32894d06bcf79aff720ef489d17453
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 17:09:16 +0400

    update

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module-1  function that returns
+True if the object is exactly an instance of the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """This function checks whether the object is an instance of some class"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
