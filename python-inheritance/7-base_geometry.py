f3b6ae8eb0f7c52d2f73552cc5e521715c201668
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 17:50:39 +0400

    task6

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module-6 class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class for BaseGeometry"""
+    def area(self):
+        """function area that raises exception"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """function that validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+            
