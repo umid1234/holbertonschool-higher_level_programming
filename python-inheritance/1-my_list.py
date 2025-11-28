88b05eeea07ea7b15ab9a75526899c5ee051e293
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 14:59:04 +0400

    task1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+class MyList(list):
+    """
+    Docstring for MyList which will print the sorted list
+    """
+    def print_sorted(self):
+        """
+        Docstring for print_sorted 
+        """
+        print(sorted(self))
