657d285766da5202eb0915cd094c83b99fc891dc
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 23:10:24 +0400

    update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module-0 eads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
     """function that reads a text file"""
     with open(filename, "r") as file:
