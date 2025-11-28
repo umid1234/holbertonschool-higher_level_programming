e784346754ab27a9415f402810aa25ff19b3cdf4
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 17:23:15 +0400

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module-6 class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class for BaseGeometry"""
+    def area(self):
+        """function area that raises exception"""
+        raise Exception("area() is not implemented")
