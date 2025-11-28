9e5d0f1269cea13ca629965c7f2dd200fc310a92
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 21:07:53 +0400

    update_task1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,4 +4,4 @@ def read_file(filename=""):
     """function that reads a text file"""
     with open(filename, "r") as file:
         content = file.read()
-        print(content)
+        print(content, end="")
