d7c3f1786ce904926f9c98bfe81563756aa86615
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 14:09:42 +0400

    update

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Module-0  
+function that returns the list of available attributes and methods of an object
+"""
 def lookup(obj):
+	"""
+	Docstring for lookup
+	the function that returns the list of availabe attributes and methods of an object
+	"""
 	return dir(obj)
