bb145859279bc42989436a81e0f64d5ecf8eb81f
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 16:36:23 +0400

    task2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Module-1  function that returns True if the object is exactly an instance of the specified class ; otherwise False
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) == a_class
