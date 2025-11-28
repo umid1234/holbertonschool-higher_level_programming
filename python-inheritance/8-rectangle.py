027815d024f7d0d9cd366dbc0b7ae2091281b36c
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-11-28 18:39:55 +0400

    task8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module-6 class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class for BaseGeometry"""
+    def area(self):
+        """function area that raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function that validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
