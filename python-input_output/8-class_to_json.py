58d7eecb720721357aa787796107947efd065092
umid1234 <50925815+umid1234@users.noreply.github.com>
2025-11-30 00:30:24 +0400

    Update 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """Module that provides a function to serialize an object's attributes."""
+
+
 def class_to_json(obj):
     """
     Return the dictionary description of an object for JSON serialization.
-
-    Args:
-        obj: The object to convert.
-
-    Returns:
-        dict: The dictionary containing the object's serializable attributes.
     """
     return obj.__dict__
